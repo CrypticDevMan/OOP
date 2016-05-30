512fed3d7a3eeed8443a1157914f87fd8009e7ec
Toxic.dev <toxic.dev@frackstudios.com>
2016-05-30 01:56:06 -0500

    adding new class.

--- src/com/frackstudios/concurrency/Main.java
+++ src/com/frackstudios/concurrency/Main.java
@@ -19,6 +19,9 @@ public class Main {
             }
         }.start();
 
+        Thread myRunnableThread = new Thread(new MyRunnable());
+        myRunnableThread.start();
+
         System.out.println("Hello again from the main thread.");
 
     }
---
+++ src/com/frackstudios/concurrency/MyRunnable.java
@@ -0,0 +1,14 @@
+package com.frackstudios.concurrency;
+
+import static com.frackstudios.concurrency.ThreadColor.ANSI_RED;
+
+/**
+ * Created by @author ToxicA0E on 5/30/2016.
+ */
+public class MyRunnable  implements  Runnable{
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_RED+"Hello from MyRunnable's implementation of run()");
+    }
+}
